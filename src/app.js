8511ae7d47e4446088dbebae8eb2b5a28b27c8a0
Ajay Poshak <poshakajay@gmail.com>
2017-08-13 22:52:22 +0530

    added logging; db queries

--- src/app.js
+++ src/app.js
@@ -4,9 +4,24 @@ const app = new Koa();
 const Router = require('koa-router');
 const router = new Router();
 
+const logger = require('koa-logger');
+const bodyParser = require('koa-bodyparser');
+
 const books = require('./controllers/books');
 
-router.get('/', books.list);
+const db = require('./db');
+const connect = db.connect(app);
+
+app.use(bodyParser());
+app.use(logger());
+
+router.get('/', async(ctx) => {
+    ctx.body = await ctx.app.booksData.find().toArray();
+});
+
+router.post('/book', async(ctx, next) => {
+    ctx.body = await ctx.app.booksData.insert(ctx.request.body, {w:1});
+});
 
 app
     .use(router.routes())
