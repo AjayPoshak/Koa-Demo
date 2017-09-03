92a6b6457b89a410df24f7d9d525d1be18c9f3c1
Ajay Poshak <poshakajay@gmail.com>
2017-09-03 12:52:06 +0530

    added createdAt field; returned the mongo response to API

--- src/modals/books.js
+++ src/modals/books.js
@@ -3,14 +3,14 @@ console.log('This is the global');
 class Books {
     listBooks(){
         console.log("Listing modal is getting called");
-       // global.booksData.find().toArray();
-	global.booksData.count();
+        return global.booksData.find().toArray();
     }
 
     addBooks(bookEntry){
         console.log(bookEntry);
         if(Object.keys(bookEntry).length > 0){
-            global.booksData.insert(bookEntry);
+            bookEntry.createdAt = new Date();
+            return global.booksData.insert(bookEntry);
         } else {
             console.error(406, 'Book details required');
         }
