c5120ca2cc8122d998e43fe3c8923834abecad92
Ajay Poshak <poshakajay@gmail.com>
2017-09-03 12:00:52 +0530

    updated db connection code

--- src/db.js
+++ src/db.js
@@ -1,13 +1,8 @@
 const MongoClient = require('mongodb').MongoClient;
 const MONGO_URL = 'mongodb://localhost:27017/books';
 
-module.exports.connect = async function(app){
-    MongoClient.connect(MONGO_URL)
-        .then((connection) => {
-            console.log('Connection to database has been established successfully');
-            app.booksData = connection.collection('booksData');
-        })
-        .catch((err) => {
-            console.error(err);
-        })
+module.exports.connect = async function(){
+    global.mongoDB = await MongoClient.connect(MONGO_URL);
+    global.booksData = global.mongoDB.collection('booksData');
+    console.log('Connection to database has been established successfully');
 }
