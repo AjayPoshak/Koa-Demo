0646efc752ec0aeceaabfe759563dc3b6cbb7944
Ajay Poshak <poshakajay@gmail.com>
2017-09-03 11:25:37 +0530

    logic moved to  odals

--- src/controllers/books.js
+++ src/controllers/books.js
@@ -1,3 +1,18 @@
-module.exports.list = async function(ctx){
-    ctx.body = 'List of books';
+const books  = require('../modals/books').getInstance();
+
+module.exports.list = async(ctx) => {
+    try {
+        console.log('This is the list');
+        ctx.body = await books.listBooks();
+    } catch(e) {
+        console.log(e);
+    }
+}
+
+module.exports.add = async(ctx, next) => {
+    try {
+        ctx.body = await books.addBooks(ctx.request.body);
+    } catch(e) {
+        console.log(e);
+    }
 }
