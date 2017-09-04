1ab4548aa1dfbed3b03353a851916abdc83ac58f
Ajay Poshak <poshakajay@gmail.com>
2017-09-04 22:22:15 +0530

    removed global; added error handling

--- src/modals/books.js
+++ src/modals/books.js
@@ -1,23 +1,26 @@
-//global.booksData = global.mongoDB.collection('booksData');
-console.log('This is the global');
 class Books {
     listBooks(){
-        console.log("Listing modal is getting called");
-        return global.booksData.find().toArray();
+        return booksData.find().toArray();
     }
 
     addBooks(bookEntry){
         console.log(bookEntry);
+
         if(Object.keys(bookEntry).length > 0){
             bookEntry.createdAt = new Date();
-            return global.booksData.insert(bookEntry);
+            return booksData.insert(bookEntry, {safe:true});
         } else {
-            console.error(406, 'Book details required');
+            return {
+                error : {
+                    message: 'Invalid data',
+                    code: 1002
+                }
+            }
         }
 
     }
 }
 
-module.exports.getInstance = function(){
+exports.getInstance = function(){
     return new Books();
 }
