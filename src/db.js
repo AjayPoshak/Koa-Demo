abe1e7d6d962483f2c0ad45ba665f4332a2f5a8a
Ajay Poshak <poshakajay@gmail.com>
2017-09-04 22:18:55 +0530

    moved and renamed as bootstrap.js

--- src/db.js
+++
@@ -1,8 +0,0 @@
-const MongoClient = require('mongodb').MongoClient;
-const MONGO_URL = 'mongodb://localhost:27017/books';
-
-module.exports.connect = async function(){
-    global.mongoDB = await MongoClient.connect(MONGO_URL);
-    global.booksData = global.mongoDB.collection('booksData');
-    console.log('Connection to database has been established successfully');
-}
